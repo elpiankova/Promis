5d6592b41e47d0615b4dba191a5874ad1e069540
len <matyevosova.lena@gmail.com>
2014-04-02 20:23:25 +0300

    played around the path of variant telemetry file

--- src/promis_api/promis_data/parsers/variant_parser.py
+++ src/promis_api/promis_data/parsers/variant_parser.py
@@ -1,14 +1,14 @@
 __author__ = 'len'
 import os, sys
-#sys.path.append("/home/len/promis/src/promis_api/")
+sys.path.append("/home/len/promis/src/promis_api/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
 
 def parse(path):
 
     path = os.path.normpath(path) # clear reduntant slashes
     (vitok_path,vitok) = os.path.split(path)
-
-
+    telemetry_file = vitok + '.txt'
+    telemetry_file = os.path.join(path, telemetry_file)
 
 
 
