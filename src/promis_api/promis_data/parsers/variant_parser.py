a9c35c80e703c11c3c3200b57ac775c8de065fc2
len <matyevosova.lena@gmail.com>
2014-05-22 19:02:39 +0300

    created a list of sampling frequencies in parser

--- src/promis_api/promis_data/parsers/variant_parser.py
+++ src/promis_api/promis_data/parsers/variant_parser.py
@@ -8,12 +8,40 @@ def parse(path):
 
     path = os.path.normpath(path) # clear reduntant slashes
     (vitok_path,vitok) = os.path.split(path)
-    telemetry_file = vitok + '.txt'
-    telemetry_file = os.path.join(path, telemetry_file)
+    telemetry_filename = vitok + '.txt'
+    telemetry_filename = os.path.join(path, telemetry_filename)
+
+    telemetry_file = open(telemetry_filename, 'r')
+    telemetry = telemetry_file.readlines()
+
+    for line in telemetry:
+        if 'Sampling frequency' in line:
+            metka_freq = telemetry.index(line)
+            print 'found'
+
+    Number_of_channels_in_table = channels_from_db.count() - 6
+    sampling_frequencies = []
+    for i in range(1, Number_of_channels_in_table+1):
+        sampling_frequencies += [telemetry[metka_freq+i]]# run through all sampling frequencies and append them to this list
+    for freq in sampling_frequencies: # delete all \n and spaces
+        sampling_frequencies[sampling_frequencies.index(freq)] = freq.strip()
+
+    for fr in sampling_frequencies:
+        if fr == '':
+            sampling_frequencies[sampling_frequencies.index(fr)] = None# replace empty lines with None
+        else:
+            sampling_frequencies[sampling_frequencies.index(fr)] = float(fr)#convert str into float number
+    print sampling_frequencies
+
+
+
+
 
 channels_from_db = Channel.objects.filter(device__satellite__title="Variant")
-print channels_from_db
+
+
+#print channels_from_db
 
 if __name__ == "__main__":
-    path = ''
+    path = '/home/len/Variant/Data_Release1/597'
     parse(path)
