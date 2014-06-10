95a1edd684ca8922f89605c6a963bc891907b029
elpiankova <el.piankova@gmail.com>
2014-06-10 17:17:14 +0300

    Added loader and some refactoring in variant parser

--- src/promis_api/promis_data/loader.py
+++ src/promis_api/promis_data/loader.py
@@ -1,7 +1,18 @@
 import os, sys
 
-
 sys.path.append("/home/elena/workspace/promis/src/promis_api/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
 
+from parsers import variant_parser
+from django.core import serializers
+    
+def loader(parser, path):
+    
+    json_generator = parser.parse(path)
+    for data in json_generator:
+        for deserialized_object in serializers.deserialize("json", data):
+            print deserialized_object.object
 
+
+path = '/home/elena/workspace/promis_from_gitlab/satellite-data/Variant/Data_Release1/597'
+loader(variant_parser, path)
--- src/promis_api/promis_data/parsers/variant_parser.py
+++ src/promis_api/promis_data/parsers/variant_parser.py
@@ -3,18 +3,17 @@ import os, sys
 import dateutil.parser
 import scipy.io
 import numpy
-sys.path.append("/home/len/promis/src/promis_api/")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
+import json
 from promis_data.models import Channel, ChannelOption
 
-def parse(path):
+sys.path.append("/home/len/promis/src/promis_api/")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
 
-    path = os.path.normpath(path) # clear redundant slashes
-    vitok = os.path.split(path)[1]
-    telemetry_filename = vitok + '.txt'
-    telemetry_filename = os.path.join(path, telemetry_filename)
 
-    telemetry_file = open(telemetry_filename, 'r')
+def parse_telemetry_file(telemetry_file):
+    """Parse info file (i.e. 597.txt) with session information.
+    Return tuple -> (begin_datetime, end_datetime, base, sampling_frequencies_list)
+    """
     telemetry = telemetry_file.read().strip().splitlines()
 
     for line in telemetry:
@@ -27,19 +26,8 @@ def parse(path):
     session_interval_strings = telemetry[-1].split('(')[0].split()
     begin_datetime = dateutil.parser.parse(session_interval_strings[0])
     end_datetime = dateutil.parser.parse(session_interval_strings[1])
-    #print session_interval_strings
-    #print begin_datetime, end_datetime
-
-    session_dict = {"model": "promis_data.session",
-                    "fields": {
-                                "time_begin": begin_datetime,
-                                "time_end": end_datetime
-                              }
-                    }
-    #print session_dict
-
-    channels_from_db = Channel.objects.filter(device__satellite__title="Variant")
-    number_of_channels_in_table = channels_from_db.count() - 6 # channels E1, E2, E3 of table can denote different electrical fields: E1,E2,E3 or E4,E5,E6 depending on base(E1 or E3)
+    
+    number_of_channels_in_table = Channel.objects.filter(device__satellite__title="Variant").count() - 6 # channels E1, E2, E3 of table can denote different electrical fields: E1,E2,E3 or E4,E5,E6 depending on base(E1 or E3)
     sampling_frequencies = []
     for i in range(number_of_channels_in_table):
         # run through all sampling frequencies and append them to this list in float
@@ -48,38 +36,56 @@ def parse(path):
             sampling_frequencies.append(None)
         else:
             sampling_frequencies.append(float(telemetry[metka_freq+i+1]))
-            
-    #print sampling_frequencies
+    
+    return (begin_datetime, end_datetime, base, sampling_frequencies)
+
+
+def parse(path):
+    
+    path = os.path.normpath(path) # clear redundant slashes
+    vitok = os.path.split(path)[1]
+    telemetry_file = open(os.path.join(path, vitok + '.txt'), 'r')
+    
+    (begin_datetime, 
+     end_datetime, 
+     base, 
+     sampling_frequencies) = parse_telemetry_file(telemetry_file)
+    
+    #print begin_datetime, end_datetime
+
+    yield json.dumps([{"model": "promis_data.session",
+                      "fields": {
+                                 "time_begin": str(begin_datetime),
+                                 "time_end": str(end_datetime)
+                                }
+                     }])
+    #print session_dict
+
+    channels_from_db = Channel.objects.filter(device__satellite__title="Variant")
 
     for channel in channels_from_db:
-            order_number = channel.channeloption_set.get(title='Order number')
-            filename = channel.channeloption_set.get(title='Filename')
+        order_number = channel.channeloption_set.get(title='Order number')
+        filename = channel.channeloption_set.get(title='Filename')
 
-            if sampling_frequencies[int(order_number.value)] != None:
-                #print options_order_number
-                print filename.value
-                #print sampling_frequencies[i]
-                if str(filename.value) + '.mat' in os.listdir(path): # mat-files should be checked first because ..
-                # 1) text files have no extension, 2) every mat-file has file with the same name and without extension
+        if sampling_frequencies[int(order_number.value)] != None:
+            #print options_order_number
+            print filename.value
+            #print sampling_frequencies[i]
+            if str(filename.value) + '.mat' in os.listdir(path): # mat-files should be checked first because ..
+            # 1) text files have no extension, 2) every mat-file has file with the same name and without extension
 
-                    measurement_file_dict = scipy.io.loadmat(os.path.join(path, str(filename.value)) + '.mat')
-                    for key in measurement_file_dict:# executes search of data and saves it in numpy.ndarray type !!!
-                        if type(measurement_file_dict[key]) == numpy.ndarray:
-                            measurement_file = measurement_file_dict[key]
-                            print type(measurement_file)# type 'numpy.ndarray'
-                elif str(filename.value) in os.listdir(path):
-                    measurement_file = open(os.path.join(path, str(filename.value)))
-                else:
-                    print 'No file "' + str(filename.value) + '" found'
-
-
-
-
-
+                measurement_file_dict = scipy.io.loadmat(os.path.join(path, str(filename.value)) + '.mat')
+                for key in measurement_file_dict:# executes search of data and saves it in numpy.ndarray type !!!
+                    if type(measurement_file_dict[key]) == numpy.ndarray:
+                        measurement_file = measurement_file_dict[key]
+                        print type(measurement_file)# type 'numpy.ndarray'
+            elif str(filename.value) in os.listdir(path):
+                measurement_file = open(os.path.join(path, str(filename.value)))
+            else:
+                print 'No file "' + str(filename.value) + '" found'
 
 
     #print ChannelOption.
-
 
 
 
