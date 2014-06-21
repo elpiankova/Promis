0e66cd016666dcf02ee175f18e13daad46c34f18
len <matyevosova.lena@gmail.com>
2014-06-21 23:00:09 +0300

    handled channels with wrong base sensor in variant_parser

--- src/promis_api/promis_data/parsers/variant_parser.py
+++ src/promis_api/promis_data/parsers/variant_parser.py
@@ -4,11 +4,12 @@ import dateutil.parser
 import scipy.io
 import numpy
 import json
-from promis_data.models import Channel, ChannelOption
 
 sys.path.append("/home/len/promis/src/promis_api/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
 
+from promis_data.models import Channel, ChannelOption
+from django.core.exceptions import ObjectDoesNotExist
 
 def parse_telemetry_file(telemetry_file):
     """Parse info file (i.e. 597.txt) with session information.
@@ -65,25 +66,40 @@ def variant_parser(path):
     channels_from_db = Channel.objects.filter(device__satellite__title="Variant")
 
     for channel in channels_from_db:
+        channel.base_sensor = 1 # this variable should exist and != None, the unity was the first choice
+        try: # check if option 'Base sensor' exists for current channel
+            channel.base_sensor = channel.channeloption_set.get(title='Base sensor').value # this can arise an exception
+            #print ' the base sensor is ' + str(channel.base_sensor)
+            if channel.base_sensor != base:
+                channel.base_sensor = None
+                #print 'Fuck this sensor'
+        except ObjectDoesNotExist:
+            pass
         channel.order_number = channel.channeloption_set.get(title='Order number').value
         channel.filename = channel.channeloption_set.get(title='Filename').value
 
         if sampling_frequencies[int(channel.order_number)] != None:
-            #print options_order_number
-            print channel.filename
-            #print sampling_frequencies[i]
-            if str(channel.filename) + '.mat' in os.listdir(path): # mat-files should be checked first because ..
-            # 1) text files have no extension, 2) every mat-file has file with the same name and without extension
+            if channel.base_sensor != None:# to skip all channels with wrong base sensor
+                print channel.filename
 
-                measurement_file_dict = scipy.io.loadmat(os.path.join(path, str(channel.filename)) + '.mat')
-                for key in measurement_file_dict:# executes search of data and saves it in numpy.ndarray type !!!
-                    if type(measurement_file_dict[key]) == numpy.ndarray:
-                        measurement_file = measurement_file_dict[key]
-                        print type(measurement_file)# type 'numpy.ndarray'
-            elif str(channel.filename) in os.listdir(path):
-                measurement_file = open(os.path.join(path, str(channel.filename)))
-            else:
-                print 'No file "' + str(channel.filename) + '" found'
+                if str(channel.filename) + '.mat' in os.listdir(path): # mat-files should be checked first because ..
+                # 1) text files have no extension, 2) every mat-file has file with the same name and without extension
+
+                    measurement_file_dict = scipy.io.loadmat(os.path.join(path, str(channel.filename)) + '.mat')
+                    for key in measurement_file_dict:# executes search of data and saves it in numpy.ndarray type !!!
+                        if type(measurement_file_dict[key]) == numpy.ndarray:
+                            measurement_file_column = measurement_file_dict[key]
+                    measurement_row = measurement_file_column.transpose()[0]
+                elif str(channel.filename) in os.listdir(path):
+                    measurement_file = open(os.path.join(path, str(channel.filename)))
+                    measurement_row = numpy.fromfile(measurement_file, dtype=numpy.float64, sep=' ')
+                else:
+                    print 'No file "' + str(channel.filename) + '" found'
+                print type(measurement_row)
+
+                for measurement in measurement_row:
+
+
 
 
     #print ChannelOption.
