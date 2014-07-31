3b0bd2436027dbd4876f6e652048c0943ca3d29d
len <matyevosova.lena@gmail.com>
2014-07-31 19:47:39 +0300

    first working version of parser for Variant data complite

--- src/promis_api/promis_data/parsers/variant_parser.py
+++ src/promis_api/promis_data/parsers/variant_parser.py
@@ -8,8 +8,9 @@ import json
 sys.path.append("/home/len/promis/src/promis_api/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
 
-from promis_data.models import Channel, ChannelOption
+from promis_data.models import Channel, ChannelOption, Parameter, ChannelsHaveParameters
 from django.core.exceptions import ObjectDoesNotExist
+from datetime import timedelta
 
 def parse_telemetry_file(telemetry_file):
     """Parse info file (i.e. 597.txt) with session information.
@@ -66,48 +67,75 @@ def variant_parser(path):
     channels_from_db = Channel.objects.filter(device__satellite__title="Variant")
 
     for channel in channels_from_db:
+
+
         channel.base_sensor = 1 # this variable should exist and != None, the unity was the first choice
         try: # check if option 'Base sensor' exists for current channel
             channel.base_sensor = channel.channeloption_set.get(title='Base sensor').value # this can arise an exception
             #print ' the base sensor is ' + str(channel.base_sensor)
             if channel.base_sensor != base:
                 channel.base_sensor = None
-                #print 'Fuck this sensor'
         except ObjectDoesNotExist:
             pass
+        
         channel.order_number = channel.channeloption_set.get(title='Order number').value
         channel.filename = channel.channeloption_set.get(title='Filename').value
 
-        if sampling_frequencies[int(channel.order_number)] != None:
-            if channel.base_sensor != None:# to skip all channels with wrong base sensor
-                print channel.filename
-
-                if str(channel.filename) + '.mat' in os.listdir(path): # mat-files should be checked first because ..
-                # 1) text files have no extension, 2) every mat-file has file with the same name and without extension
-
-                    measurement_file_dict = scipy.io.loadmat(os.path.join(path, str(channel.filename)) + '.mat')
-                    for key in measurement_file_dict:# executes search of data and saves it in numpy.ndarray type !!!
-                        if type(measurement_file_dict[key]) == numpy.ndarray:
-                            measurement_file_column = measurement_file_dict[key]
-                    measurement_row = measurement_file_column.transpose()[0]
-                elif str(channel.filename) in os.listdir(path):
-                    measurement_file = open(os.path.join(path, str(channel.filename)))
-                    measurement_row = numpy.fromfile(measurement_file, dtype=numpy.float64, sep=' ')
-                else:
-                    print 'No file "' + str(channel.filename) + '" found'
-                print type(measurement_row)
-
-                for measurement in measurement_row:
+        for p in channel.parameters.all():
+            if not p.parents.all():
+                parameter_name = p.title
 
 
+        if sampling_frequencies[int(channel.order_number)] != None and channel.base_sensor != None:
+        #                                                              to skip all channels with wrong base sensor
+            print channel.filename
+
+            if str(channel.filename) + '.mat' in os.listdir(path): # mat-files should be checked first because ..
+            # 1) text files have no extension, 2) every mat-file has file with the same name and without extension
+
+                measurement_file_dict = scipy.io.loadmat(os.path.join(path, str(channel.filename)) + '.mat')
+                for key in measurement_file_dict:# executes search of data and saves it in numpy.ndarray type
+                    if type(measurement_file_dict[key]) == numpy.ndarray:
+                        measurement_file_column = measurement_file_dict[key]
+                measurement_row = measurement_file_column.transpose()[0]
+            elif str(channel.filename) in os.listdir(path):
+                measurement_file = open(os.path.join(path, str(channel.filename)))
+                measurement_row = numpy.fromfile(measurement_file, dtype=numpy.float64, sep=' ')
+            else:
+                print 'No file "' + str(channel.filename) + '" found'
+            print type(measurement_row)
+
+            period_microsec =  timedelta(microseconds=1/sampling_frequencies[int(channel.order_number)]*10**6)
+            measurement_datetime = begin_datetime
+
+            for measurement in measurement_row:
+
+                if measurement != 0: #
 
 
-    #print ChannelOption.
+                    yield json.dumps([{"model": "promis_data.measurement",
+                          "fields": {
+                                    "level_marker": 0,
+                                    "measurement": measurement,
+                                    "parameter": str(parameter_name),
+                                    "channel": [str(channel.title), str(channel.device.title)],
+                                    "measurement_point": [str(measurement_datetime)],
+                                    "session": [str(begin_datetime), str(end_datetime)]
+                                    }
+                         }])
+
+                    measurement_datetime += period_microsec
+                    pass
 
 
 
 
-    #print channels_from_db
+#print ChannelOption.
+
+
+
+
+#print channels_from_db
 
 if __name__ == "__main__":
 #     path = '/home/elena/workspace/promis_from_gitlab/satellite-data/Variant/Data_Release1/597'
@@ -115,5 +143,8 @@ if __name__ == "__main__":
     gen = variant_parser(path)
     print next(gen)
     print next(gen)
-    
+    print next(gen)
+
+
+
     
