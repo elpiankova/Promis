f0dd0e5051e857001c749c5fa61a3fb9020fb4d0
elpiankova <el.piankova@gmail.com>
2014-08-19 21:24:11 +0300

    Added some urls

--- src/promis_api/promis_data/urls.py
+++ src/promis_api/promis_data/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from promis_data import views
+from django.conf import settings
 
 
 urlpatterns = patterns('',
+   (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'sessions/$', views.SessionList.as_view()),
     url(r'measurement_points/$', views.MeasurementPointList.as_view()),
     url(r'measurements/$', views.MeasurementList.as_view())
