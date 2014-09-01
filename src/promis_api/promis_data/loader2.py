2854f7341ad31c776ca88adc3472339072dfcae1
len <matyevosova.lena@gmail.com>
2014-09-01 23:26:06 +0300

    Created new files, variant_parser2 and loader2, which implement parsing with object.create() method

--- src/promis_api/promis_data/loader2.py
+++ src/promis_api/promis_data/loader2.py
@@ -2,34 +2,29 @@ import os
 import sys
 import logging
 
-
 sys.path.append("/home/elena/workspace/promis/src/promis_api/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
 
-from parsers import variant_parser, potential_parser
-from django.core import serializers
+from parsers import variant_parser2, potential_parser
+#from django.core import serializers
 from django.db.utils import IntegrityError
 from datetime import *
-import time
+#import time
 
-#sys.stdout = open('logg', 'w')
 logging.basicConfig(level=logging.INFO,  # DEBUG massages will be omitted
                     filename=str(str(datetime.now()) + '.log'),
                     filemode='w',
                     format='%(asctime)s %(lineno)s %(levelname)-8s %(message)s')
 
 def loader(parser, path):
-    
-    json_generator = parser.parser(path)
-    for data in json_generator:
-        for deserialized_object in serializers.deserialize("json", data):
-            try:
-                deserialized_object.object.save()
-            except IntegrityError:
-                logging.debug("%s already exists" % str(deserialized_object.object))  # now omitted
-            except Exception, e:
-                raise Exception(e)
-                
+
+    try:
+        parser.parser(path)
+    except IntegrityError:
+        logging.debug("already exists")  # now omitted
+    except Exception, e:
+        raise Exception(e)
+
 path_variant = '/home/len/Variant/Data_Release1'
 #path = '/home/elena/workspace/Data_Release1_2_session/597/'
 dirs_variant = []
@@ -38,11 +33,9 @@ for folder_name in next(os.walk(path_variant))[1]:
 #print dirs
 
 for path in dirs_variant:
-    start = time.clock()
     logging.info('%s directory of Variant project is loading' % path)
-    loader(variant_parser, path)
+    loader(variant_parser2, path)
     logging.info('%s directory of Variant project has been loaded' % path)
-    logging.info('Vitok time: %s' % (time.clock() - start))
 
 path_potential = '/home/len/Potential/DECODED'
 dirs_potential = []
@@ -53,4 +46,5 @@ for folder in next(os.walk(path_potential))[1]:
 
 for path in dirs_potential:
     logging.info('%s directory of Potential project is loading' % path)
-    #loader(potential_parser, path)
+    loader(potential_parser, path)
+    logging.info('%s directory of Potential project has been loaded' % path)
--- src/promis_api/promis_data/parsers/variant_parser2.py
+++ src/promis_api/promis_data/parsers/variant_parser2.py
@@ -3,20 +3,24 @@ import os, sys
 import dateutil.parser
 import scipy.io
 import numpy
-import json
 import pytz
 import logging
 import cProfile, pstats, StringIO
 
 pr = cProfile.Profile()
 
-
 sys.path.append("/home/len/promis/src/promis_api/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "promis_api.settings")
 
-from promis_data.models import Channel, ChannelOption, Parameter, ChannelsHaveParameters
+from promis_data.models import Channel, ChannelOption, Parameter, ChannelsHaveParameters, Session, MeasurementPoint, Measurement
 from django.core.exceptions import ObjectDoesNotExist
-from datetime import timedelta
+from datetime import timedelta,datetime
+from django.db.utils import IntegrityError
+
+logging.basicConfig(level=logging.INFO,  # DEBUG massages will be omitted
+                    filename=str(str(datetime.now()) + '.log'),
+                    filemode='w',
+                    format='%(asctime)s %(lineno)s %(levelname)-8s %(message)s')
 
 def parse_telemetry_file(telemetry_file):
     """Parse info file (i.e. 597.txt) with session information.
@@ -35,7 +39,7 @@ def parse_telemetry_file(telemetry_file):
     session_interval_strings = telemetry[-1].split('(')[0].split()
     begin_datetime = dateutil.parser.parse(session_interval_strings[0])
     end_datetime = dateutil.parser.parse(session_interval_strings[1])
-    
+
     number_of_channels_in_table = Channel.objects.filter(device__satellite__title="Variant").count() - 6  # channels E1, E2, E3 of table can denote different electrical fields: E1,E2,E3 or E4,E5,E6 depending on base(E1 or E3)
     sampling_frequencies = []
     for i in range(number_of_channels_in_table):
@@ -45,7 +49,7 @@ def parse_telemetry_file(telemetry_file):
             sampling_frequencies.append(None)
         else:
             sampling_frequencies.append(float(telemetry[metka_freq+i+1]))
-    
+
     return (begin_datetime, end_datetime, base, sampling_frequencies)
 
 
@@ -56,32 +60,31 @@ def parser(path):
     path = os.path.normpath(path) # clear redundant slashes
     vitok = os.path.split(path)[1]
     telemetry_file = open(os.path.join(path, vitok + '.txt'), 'r')
-    
-    (begin_datetime, 
-     end_datetime, 
-     base, 
+
+    (begin_datetime,
+     end_datetime,
+     base,
      sampling_frequencies) = parse_telemetry_file(telemetry_file)
 
     if not telemetry_file.closed:
         telemetry_file.close()
-    
+
     #print begin_datetime, end_datetime
-    
+
     begin_datetime = pytz.utc.localize(begin_datetime) - timedelta(hours=3)
     end_datetime = pytz.utc.localize(end_datetime) - timedelta(hours=3)
-
-    yield json.dumps([{"model": "promis_data.session",
-                      "fields": {
-                                 "time_begin": str(begin_datetime),
-                                 "time_end": str(end_datetime)
-                                }
-                     }])
+    try:
+        Session.objects.create(time_begin=begin_datetime, time_end=end_datetime)
+        logging.info('Session %s - %s has been created successfully' % (begin_datetime, end_datetime))
+    except IntegrityError:
+        logging.warning('session %s - %s already exists' % (begin_datetime, end_datetime))
+    session = Session.objects.get(time_begin=begin_datetime, time_end=end_datetime)
 
     channels_from_db = Channel.objects.filter(device__satellite__title="Variant")
 
     for channel in channels_from_db:
 
-        channel.base_sensor = 1 # this variable should exist and != None, the unity was the first choice
+        channel.base_sensor = 1  # this variable should exist and != None, the unity was the first choice
         try: # check if option 'Base sensor' exists for current channel
             channel.base_sensor = channel.channeloption_set.get(title='Base sensor').value  # this can arise an exception
             #print ' the base sensor is ' + str(channel.base_sensor)
@@ -89,23 +92,23 @@ def parser(path):
                 channel.base_sensor = None
         except ObjectDoesNotExist:
             pass
-        
+        print channel.title
         channel.order_number = channel.channeloption_set.get(title='Order number').value
         channel.filename = channel.channeloption_set.get(title='Filename').value
-        channel.conv_factor = float(channel.channeloption_set.get(title='Conversion factor').value)
+        try:
+            channel.conv_factor = 1/float(channel.channeloption_set.get(title='Conversion factor').value)
+        except ChannelOption.DoesNotExist:
+            channel.conv_factor = 1
 
         for p in channel.parameters.all():
             if not p.parents.all():
-                parameter_name = p.title
+                parameter = p
         count = 0  # for counting measurements within one channel
         measurement_row = []  #
         if sampling_frequencies[int(channel.order_number)] is not None and channel.base_sensor is not None:
         #                                                              to skip all channels with wrong base sensor
-            logging.info('%s channel is to be loaded' % channel.filename)
-
             if str(channel.filename) + '.mat' in os.listdir(path):  # mat-files should be checked first because ..
             # 1) text files have no extension, 2) every mat-file has file with the same name and without extension
-
                 measurement_file_dict = scipy.io.loadmat(os.path.join(path, str(channel.filename)) + '.mat')
                 for key in measurement_file_dict:  # executes search of data and saves it in numpy.ndarray type
                     if type(measurement_file_dict[key]) == numpy.ndarray:
@@ -120,48 +123,41 @@ def parser(path):
                 logging.warning('No file %s found' % channel.filename)
             #print type(measurement_file_dict)
 
+            logging.info('%s channel is to be loaded with %s measurements' % (channel.filename, len(measurement_row)))
+
             period_microsec = timedelta(microseconds=1/sampling_frequencies[int(channel.order_number)]*10**6)
             measurement_datetime = begin_datetime
+            if len(measurement_row) < 10000:
+                for measurement in measurement_row:
+                    #pr.enable()
+                    if measurement != 0:
 
-            block_of_meas_times = []
-            block_of_meas = []
-            for measurement in measurement_row:
-                pr.enable()
-                if measurement != 0:
+                        measurement *= channel.conv_factor
+                        try:
+                            MeasurementPoint.objects.create(time=measurement_datetime)
+                        except IntegrityError:
+                            logging.warning('Measurement point %s already exists', measurement_datetime)
 
-                    measurement /= channel.conv_factor
+                        meas_point = MeasurementPoint.objects.get(time=measurement_datetime)
 
-                    block_of_meas_times.append({"model": "promis_data.measurementpoint",
-                                                "fields": {
-                                                "time": str(measurement_datetime)
-                                                           }
-                                                })
-                    block_of_meas.append({"model": "promis_data.measurement",
-                                       "fields": {
-                                                  "level_marker": 0,
-                                                  "measurement": measurement,
-                                                  "parameter": str(parameter_name),
-                                                  "channel": [str(channel.title), str(channel.device.title)],
-                                                  "measurement_point": [str(measurement_datetime)],
-                                                  "session": [str(begin_datetime), str(end_datetime)]
-                                                  }
-                                         })
+                        try:
+                            Measurement.objects.create(level_marker=0,
+                                                       measurement=measurement,
+                                                       parameter=parameter,
+                                                       channel=channel,
+                                                       measurement_point=meas_point,
+                                                       session=session)
+                        except IntegrityError:
+                            logging.warning('Measurement %s of %s channel already exists', measurement_datetime, channel)
+                        measurement_datetime += period_microsec
+                        count += 1
+                    #pr.disable()
+                    #s = StringIO.StringIO()
+                    #sortby = 'cumulative'
+                    #ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+                    #ps.print_stats()
+                    #logging.info('%s' % s.getvalue())
 
-                    measurement_datetime += period_microsec
-                    count += 1
-                    if count%100 == 0:
-                        yield json.dumps(block_of_meas_times)
-                        block_of_meas_times=[]
-                        yield json.dumps(block_of_meas)
-                        block_of_meas = []
-                pr.disable()
-                s = StringIO.StringIO()
-                sortby = 'cumulative'
-                ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-                ps.print_stats()
-                logging.info('%s' % s.getvalue())
-        yield json.dumps(block_of_meas_times)
-        yield json.dumps(block_of_meas)
         logging.info('%s channel has been loaded with %s measurements', channel.filename, count)
 
 
@@ -171,8 +167,6 @@ if __name__ == "__main__":
     import timeit
     path = '/home/len/Variant/Data_Release1/1056'
 #    path = '/home/elena/workspace/promis_from_gitlab/satellite-data/Variant/Data_Release1/597'
-    gen = parser(path)
-    print next(gen)
-    print next(gen)
-    print next(gen)
+    parser(path)
+
     print timeit.timeit('parser("/home/len/Variant/Data_Release1/1056")', 'from __main__ import parser')
