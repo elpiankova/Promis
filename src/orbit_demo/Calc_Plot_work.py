68332a6a02dfff516487e3eaa798acb88cb405e4
serg_yakim <serg.yakimov@gmail.com>
2014-09-07 08:06:22 +0300

    Last version of plot file

--- src/orbit_demo/Calc_Plot_work.py
+++ src/orbit_demo/Calc_Plot_work.py
@@ -21,7 +21,7 @@ T = 2456896.507593647577 #(Julian day number), текущее время
 i = 0
 Lon = []
 Lat = []
-time = 300
+time = 3000
 while i <= time:
     E = 0
     E0 = 0.5 # произвольное начальное значение экцентрической аномалии
@@ -44,8 +44,7 @@ while i <= time:
     X0 = a*(cos(E*pi/180) - e)
     Y0 = a*sqrt(1-e**2)*sin(E*pi/180)
     Z0 = 0
-#            print('X0 = ',X0,' Y0 = ',Y0, ' Z0 = ',Z0)
-            # расчет декартовых координат КА
+# расчет декартовых координат КА
     X = (cos(omega_big)*cos(omega_small)-sin(omega_big)*cos(I)*sin(omega_small))*X0 - \
         (cos(omega_big)*sin(omega_small)+sin(omega_big)*cos(I)*cos(omega_small))*Y0 + \
         sin(omega_big)*sin(I)*Z0
@@ -53,27 +52,53 @@ while i <= time:
         (sin(omega_small)*sin(omega_big)-cos(omega_big)*cos(I)*cos(omega_small))*Y0 - \
         cos(omega_big)*sin(I)*Z0
     Z = sin(I)*sin(omega_small)*X0 + sin(I)*cos(omega_small)*Y0 + cos(I)*Z0
-#    print(X, Y, Z) 
-            # переход из декартовых в сферические координаты
-    fi = (180/pi)*atan2(Y, X) - 0.25/2*i # долгота
+# переход из декартовых в сферические координаты
+    fi = (180/pi)*atan2(Y, X) - 0.25*i/8 # долгота
     teta = (180/pi)*atan2(Z, sqrt(X**2 + Y**2)) # широта
-#            print(fi, teta)
+
     if fi < -180:
         fi = 180 - (abs(fi) - 180)
 
     Lon.append(fi)
     Lat.append(teta)
-    print(Lon[i], Lat[i])
-    i += 1
-    T += 0.000694444/2
-  
-print(Lon)
-print(Lat)    
-Lon1 = Lon[0:177]
-Lat1 = Lat[0:177]
-Lon2 = Lon[178:300]
-Lat2 = Lat[178:300]
 
+    i += 1
+    T += 0.000694444/8 # дискредетация 1/8 минуты
+
+Lon2 = []
+Lat2 = []
+
+while len(Lon) > 0:
+        
+    begin = 0
+    end = 0
+    end1 = 0
+
+    for i in range(len(Lon)):
+        if end == 0:
+            if Lon[i] < 180 and Lon[i] > 0:
+                end = i
+            else: pass
+        else: pass
+
+    Lon1 = Lon[end:]
+
+    for i in range(len(Lon1)):
+        if end1 == 0:
+            if Lon1[i] < 0 or Lon1[i] == Lon1[-1] :
+                end1 = i
+            else: pass
+            if Lon1[i] == Lon1[-1]:
+                end += 1
+            else: pass
+        else: pass    
+    end = end1 + end 
+    
+    Lon2.append(Lon[begin:end])
+    Lat2.append(Lat[begin:end])
+
+    Lon = Lon[end:]
+    Lat = Lat[end:]
     
 # Отрисовка карты Земли с трассой спутника
 
@@ -90,10 +115,8 @@ map.drawmapboundary()
 map.drawmeridians(np.arange(0, 360, 30))
 map.drawparallels(np.arange(-90, 90, 30))
 
-x, y = map(Lon1, Lat1)
-x1, y1 = map(Lon2, Lat2)
-
-map.plot(x, y, '-', markersize=1, linewidth=2, color='b', markerfacecolor='b')
-map.plot(x1, y1, '-', markersize=1, linewidth=2, color='b', markerfacecolor='b')
+for i in range(len(Lon2)) and range(len(Lat2)):    
+    x, y = map(Lon2[i], Lat2[i])
+    map.plot(x, y, '-', markersize=1, linewidth=2, color='b', markerfacecolor='b')
 
 plt.show()
