40d21009ccabebe1ec47f137287d588017074a97
mammut <saniaboy@yandex.ru>
2015-07-08 16:22:51 +0300

    Добавлено кнопку завантаження і автозаповнення перших двох полів

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,45 +1,11 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
+var requestKNA = angular.module('requestKNA', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope, $http){
-
-    //Масив даних
-    $scope.title = "Телефони";
-    $http.get('phones/phones.json').succsess(function() {
-
-    }).error()
-
-    //Дата
-    var date = new Date();
-    $scope.today = date;
-
-    //Фільтр
-    $scope.doneAddFilter = function(phoneItem) {
-        return phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true;
-    }
-
-    //Сортування
-    $scope.sortFiled = undefined;
-    $scope.reverse = false;
-
-    $scope.sort = function(filedName){
-        if ($scope.sortFiled === filedName) {
-            $scope.sortFiled = !$scope.reverse;
+requestKNA.controller('СlaimInfoCtrl', function($scope){
+    $scope.maindata =
+        {   'kna' : 'KNA',
+            'appnumber' : '0003'
         }
-        else{
-            $scope.sortFiled = filedName;
-            $scope.reverse = false;
-        }
-    };
-
-    //Іконка сортування яка показує напрямок сортування файлів
-
-    $scope.isSortDown = function(filedName){
-        return $scope.sortFiled === filedName && $scope.reverse;
-    };
-    $scope.isSortUp = function(filedName){
-        return $scope.sortFiled === filedName && !$scope.reverse;
-    };
 });
