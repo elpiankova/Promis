9c71c15a9450426517b76d771334c56f19722b49
mammut <mammut@debian>
2015-07-23 13:39:05 +0300

    Версія для смільної роботи

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,22 +4,12 @@
 requestKNA.controller('claimInfoCtrl',['$scope','$http', function($scope, $http) {
     $http.get('behavior/appliance.json').success(function(data, status, headers, config) {
         $scope.appliance = data;
+        console.log(data);
     });
-
-    $http.get('behavior/behavior.json').success(function(data, status, headers, config) {
-        $scope.behavior = data;
-    });
-
     $http.get('behavior/branches.json').success(function(data, status, headers, config) {
         $scope.branches = data;
         console.log(data);
     });
-
-    $scope.numberval = '1';
-    $scope.startPointval = '1';
-
-    $scope.borderLeftval = '1';
-    $scope.borderRightval = '1';
 }]);
 
 requestKNA.controller('TodoListController', function() {
