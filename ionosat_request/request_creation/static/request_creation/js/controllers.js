c377d67d345f613605e55db9dee6715a44970cf5
mammut <mammut@debian>
2015-07-23 17:22:24 +0300

    Логи для get number

--- ionosat_request/request_creation/static/request_creation/js/controllers.js
+++ ionosat_request/request_creation/static/request_creation/js/controllers.js
@@ -5,9 +5,23 @@ requestKNA.controller('claimInfoCtrl',['$scope','$http', function($scope, $http)
     $http.get('/devices').success(function(data, status, headers, config) {
         $scope.appliance = data;
     });
-    $http.get('behavior/branches.json').success(function(data, status, headers, config) {
+    $http.get('/orbit_flag').success(function(data, status, headers, config) {
         $scope.branches = data;
     });
+
+    $http.get('/number').success(function(data, status, headers, config) {
+        $scope.branches = data;
+        console.log(data);
+        console.log(status);
+        console.log(headers);
+        console.log(config);
+    }).
+  error(function(data, status, headers, config) {
+        console.log(status);
+        console.log(headers);
+        console.log(config);
+  });
+
 }]);
 
 requestKNA.controller('TodoListController', function() {
