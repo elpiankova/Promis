ee22d6a59e9dc1f82620c17f99fe8642d9e2181d
mammut <mammut@debian>
2015-07-24 12:34:03 +0300

    Всі get запити приходять і все відображається

--- ionosat_request/request_creation/static/request_creation/js/controllers.js
+++ ionosat_request/request_creation/static/request_creation/js/controllers.js
@@ -7,19 +7,12 @@ requestKNA.controller('claimInfoCtrl',['$scope','$http', function($scope, $http)
     });
     $http.get('/orbit_flag').success(function(data, status, headers, config) {
         $scope.branches = data;
+
+        console.log(data);
     });
 
     $http.get('/number').success(function(data, status, headers, config) {
-        $scope.branches = data;
-        console.log(data);
-        console.log(status);
-        console.log(headers);
-        console.log(config);
-    }).
-  error(function(data, status, headers, config) {
-        console.log(status);
-        console.log(headers);
-        console.log(config);
+        $scope.number = data;
   });
 
 }]);
