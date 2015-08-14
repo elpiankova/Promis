4a21246cf3612f05f391c393e7577358ae7efd68
Mammut <mammutcris@gmail.com>
2015-08-14 14:08:15 +0300

    time mask is work

--- ionosat_request/request_creation/static/request_creation/js/directives.js
+++ ionosat_request/request_creation/static/request_creation/js/directives.js
@@ -40,37 +40,9 @@ requestKNA.directive(
 		return {
 			require: 'ngModel',
 			link: function (scope, element, attrs, ngModelCtrl) {
-				$.widget(
-					"ui.timespinner", $.ui.spinner, {
-						options: {
-							// seconds
-							step: 60 * 1000,
-							// hours
-							page: 60
-						},
-
-						_parse: function (value) {
-							if (typeof value === "string") {
-								// already a timestamp
-								if (Number(value) == value) {
-									return Number(value);
-								}
-								return +Globalize.parseDate(value);
-							}
-							return value;
-						},
-
-						_format: function (value) {
-							return Globalize.format(new Date(value), "t", "de-DE");
-						}
-					}
-				);
-
-				$(
-					function () {
-						$("#spinner").timespinner();
-						Globalize.culture("de-DE");
-
+				jQuery(
+					function ($) {
+						$(".fromtime").mask("99:99:99");
 					}
 				);
 			}
