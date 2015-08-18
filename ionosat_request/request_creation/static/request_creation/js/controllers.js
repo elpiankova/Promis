4e15726c1fd4d21c1e7e35f90c218189043b7dee
Mammut <mammutcris@gmail.com>
2015-08-18 16:22:15 +0300

    Made fixed menu for general power and volume data

--- ionosat_request/request_creation/static/request_creation/js/controllers.js
+++ ionosat_request/request_creation/static/request_creation/js/controllers.js
@@ -26,6 +26,43 @@ requestKNA.controller(
 			}
 		);
 
+		var timeModes = function () {
+
+			var index;
+			for (index = 0; index < claim.todos.length; ++index) {
+				var time_modes = claim.todos[index].time_duration.split(':');
+				var hh = Number(time_modes[0]);
+				var mm = Number(time_modes[1]);
+				var ss = Number(time_modes[2]);
+				var time = hh * 3600 + mm * 60 + ss;
+				var index_device;
+				for (index_device = 0; index_device < claim.appliance.length; ++index_device) {
+					if (claim.todos[index].device == claim.appliance[index_device].name) {
+						var modes = claim.appliance[index_device].modes;
+						var index_modes;
+						for (
+							index_modes = 0; index_modes < modes.length;
+							++index_modes
+						) {
+							if (claim.todos[index].mode == modes[index_modes].name) {
+
+								var data_amount = modes[index_modes].data_speed * time / 8;
+								claim.todos[index]["data_amount"] = data_amount;
+								claim.todos[index].data_amount = data_amount;
+								claim.data_amount = data_amount;
+								console.log(claim.data_amount);
+								claim.Reqest = true;
+								var power_amount = modes[index_modes].power * time / 3600;
+								claim.todos[index]["power_amount"] = power_amount;
+								claim.todos[index].power_amount = power_amount;
+								claim.power_amount = power_amount;
+								console.log(claim.power_amount);
+							}
+						}
+					}
+				}
+			}
+		};
 		claim.todos = [];
 		claim.addTodo = function () {
 			claim.todos.push({});
@@ -33,7 +70,6 @@ requestKNA.controller(
 		};
 
 		claim.dataReqest = function () {
-
 			var request = claim.user;
 			console.log(request);
 			var index;
@@ -62,39 +98,7 @@ requestKNA.controller(
 				}
 			);
 		};
-		claim.timeModes = function () {
-
-			var index;
-			for (index = 0; index < claim.todos.length; ++index) {
-				var time_modes = claim.todos[index].time_duration.split(':');
-				var hh = Number(time_modes[0]);
-				var mm = Number(time_modes[1]);
-				var ss = Number(time_modes[2]);
-				var time = hh * 3600 + mm * 60 + ss;
-				var index_device;
-				for (index_device = 0; index_device < claim.appliance.length; ++index_device) {
-					if (claim.todos[index].device == claim.appliance[index_device].name) {
-						var modes = claim.appliance[index_device].modes;
-						var index_modes;
-						for (
-							index_modes = 0; index_modes < modes.length;
-							++index_modes
-						) {
-							if (claim.todos[index].mode == modes[index_modes].name) {
-
-								var data_amount = modes[index_modes].data_speed * time / 8;
-								claim.todos[index]["data_amount"] = data_amount;
-								claim.todos[index].data_amount = data_amount;
-								claim.Reqest = true;
-								var power_amount = modes[index_modes].power * time / 3600;
-								claim.todos[index]["power_amount"] = power_amount;
-								claim.todos[index].power_amount = power_amount;
-							}
-						}
-					}
-				}
-			}
-		};
+		claim.timeModes = timeModes;
 	}
 );
 
