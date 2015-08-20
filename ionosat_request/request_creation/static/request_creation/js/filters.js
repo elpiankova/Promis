a791baf5cc43ece4c99b76c6f581efdb0006920a
Mammut <mammutcris@gmail.com>
2015-08-20 17:19:07 +0300

    mini works

--- ionosat_request/request_creation/static/request_creation/js/filters.js
+++ ionosat_request/request_creation/static/request_creation/js/filters.js
@@ -14,18 +14,18 @@ requestKNA.filter (
 			else if ( name <= Mi ) {
 				text_data_amount = name / Ki;
 				text_data_amount = text_data_amount.toFixed ( 1 );
-				text_data_amount = text_data_amount + " Килобайт";
+				text_data_amount = text_data_amount + " Кбайт";
 			}
 			else if ( name <= Gi ) {
 				text_data_amount = name / Mi;
 				text_data_amount = text_data_amount.toFixed ( 1 );
-				text_data_amount = text_data_amount + " Мегабайт";
+				text_data_amount = text_data_amount + " Мбайт";
 
 			}
 			else if ( name > Gi ) {
 				text_data_amount = name / Gi;
 				text_data_amount = text_data_amount.toFixed ( 1 );
-				text_data_amount = text_data_amount + " Гигиабайт";
+				text_data_amount = text_data_amount + " Гбайт";
 
 			}
 			return text_data_amount;
