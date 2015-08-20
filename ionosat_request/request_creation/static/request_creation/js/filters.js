6870b4ccb09eefd344d854d650bfbc833451aa4a
Mammut <mammutcris@gmail.com>
2015-08-20 16:55:45 +0300

    Filter works

--- ionosat_request/request_creation/static/request_creation/js/filters.js
+++ ionosat_request/request_creation/static/request_creation/js/filters.js
@@ -11,12 +11,12 @@ requestKNA.filter (
 			if ( name <= Ki ) {
 				text_data_amount = name + " Байт";
 			}
-			else if ( name > Ki ) {
-				text_data_amount = name / Mi;
+			else if ( name <= Mi ) {
+				text_data_amount = name / Ki;
 				text_data_amount = text_data_amount.toFixed ( 1 );
 				text_data_amount = text_data_amount + " Килобайт";
 			}
-			else if ( name > Mi ) {
+			else if ( name <= Gi ) {
 				text_data_amount = name / Mi;
 				text_data_amount = text_data_amount.toFixed ( 1 );
 				text_data_amount = text_data_amount + " Мегабайт";
