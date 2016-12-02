8cfbeee8d914f97cd093734cdc1fda6e492b5655
elpiankova <el.piankova@gmail.com>
2016-12-02 13:17:02 +0200

    Fixed Decimal limit to longitudes

--- ionosat_request/request_creation/validators.py
+++ ionosat_request/request_creation/validators.py
@@ -1,13 +1,14 @@
 
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
+from decimal import Decimal
 
 
 request_max_number_validator = MaxValueValidator(9999, message='Request number must be less than 9999')
-request_max_latitude_start_validator = MaxValueValidator(82.0, message='Request latitude start must be less then 82.0')
-request_min_latitude_start_validator = MinValueValidator(-82.0,
+request_max_latitude_start_validator = MaxValueValidator(Decimal('82.0'), message='Request latitude start must be less then 82.0')
+request_min_latitude_start_validator = MinValueValidator(Decimal('-82.0'),
                                                          message='Request latitude start must be greater than -82.0')
-request_max_longitude_validator = MaxValueValidator(359.9, message='Request longitude stop must be less then 359.9')
-request_min_longitude_validator = MinValueValidator(000.0,
+request_max_longitude_validator = MaxValueValidator(Decimal('359.9'), message='Request longitude stop must be less then 359.9')
+request_min_longitude_validator = MinValueValidator(Decimal('000.0'),
                                                     message='Request longitude start must be greater then 000.0')
 device_amount_max_val_validator = MaxValueValidator(8, message='Request device amount must be in range 1-8')
 device_amount_min_val_validator = MinValueValidator(0, message='Request device amount must be in range 1-8')
